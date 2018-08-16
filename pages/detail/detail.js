6688594f480b4dd646fdbd9d03598537ed21f5c6
zhibin <xie1776@126.com>
2018-08-16 20:20:46 +0800

    v1.0

---
+++ pages/detail/detail.js
@@ -0,0 +1,37 @@
+
+var md5 = require('../../utils/md5.js');
+var app = getApp()
+Page({
+  onLoad: function (option) {
+    var id = option.id;
+    var that = this
+    console.log(option.id);
+    wx.request({
+      url: md5.sign('https://api.xwta.net/Joke/getDetailImg'),
+      data: {
+        id: id
+      },
+      header: {
+        'contentType': 'application/json'
+      },
+      success: function (res) {
+        console.log(res.data)
+        if (res.data.code) {
+          that.setData({
+            info: res.data.info
+          })
+        } else {
+          console.log('fail');
+        }
+      }
+    })
+  },
+  onShareAppMessage: function (e) { //分享
+    // console.log(e);
+    return {
+      title: '希望塔娱乐',
+      desc: '希望塔致力于做你休闲时光的伴侣!',
+      path: '/page/detail/detail?id='+id
+    }
+  },
+});
---
+++ pages/girl/index.js
@@ -0,0 +1,79 @@
+
+var md5 = require('../../utils/md5.js');
+
+//请求数据
+var list = [];
+function getJokeList(that, p = 1) {
+  wx.request({
+    url: md5.sign('https://api.xwta.net/Girl/getListByImg'),
+    data: {
+      p: p
+    },
+    header: {
+      'contentType': 'application/json'
+    },
+    success: function (res) {
+      console.log(res.data)
+      if (res.data.code) {
+
+        for (var i = 0; i < res.data.list.length; i++) {
+          list.push(res.data.list[i]);
+        }
+        that.setData({
+          jokeList: list
+        })
+      } else {
+        console.log('fail');
+      }
+    }
+  })
+  // showRequestInfo()
+}
+
+var p = 1
+var app = getApp()
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    jokeList: [],
+  },
+  onLoad: function () {
+    console.log('onLoad');
+    console.log(app.globalData);
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function (userInfo) {
+      //更新数据
+      that.setData({
+        userInfo: userInfo
+      })
+    })
+    getJokeList(that);
+  },
+  onReachBottom: function (e) { //onReachBottom
+    p = p + 1;
+    var that = this
+    wx.showToast({ //期间为了显示效果可以添加一个过度的弹出框提示“加载中”  
+      title: '加载中',
+      icon: 'loading',
+      duration: 1000
+    })
+    getJokeList(that, p);
+  },
+  onShareAppMessage: function () { //分享
+    return {
+      title: '希望塔娱乐',
+      desc: '希望塔致力于做你休闲时光的伴侣!',
+      path: '/page/index/index'
+    }
+  },
+  bindViewTap: function (e) {
+    var id = e.currentTarget.dataset.id;
+    console.log(id);
+    wx.navigateTo({
+      url: '../detail/detail?id=' + id,
+    })
+  }
+
+})
